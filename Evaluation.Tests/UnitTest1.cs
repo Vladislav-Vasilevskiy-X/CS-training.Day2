4be021c5cfa48caecd400217c3326d13f693fc64
Vladislav <Uladzislau_Parkhutsich@epam.com>
2015-12-10 13:12:39 +0300

    task1Done

---
+++ Evaluation.Tests/UnitTest1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static Evaluation.Calculation;
+
+namespace Evaluation.Tests
+{
+    [TestClass]
+    public class CalculationTests
+    {
+        [TestMethod]
+        public void Evaluation_NegNumberPosPowerPosAccuracy_Zero()
+        {
+            Assert.AreEqual(0.0, Evaluate(-1, 2, 1));
+        }
+
+        [TestMethod]
+        public void Evaluation_PosNumberPosPowerNegAccuracy_Zero()
+        {
+            Assert.AreEqual(0.0, Evaluate(2, 2, -1));
+        }
+
+        [TestMethod]
+        public void Evaluation_PosNumberNegPowerPosAccuracy_Zero()
+        {
+            Assert.AreEqual(0.0, Evaluate(2, -2, 1));
+        }
+
+        [TestMethod]
+        public void Evaluation_PosNumberPosPowerWriteAccuracy_Result()
+        {
+            Assert.AreEqual(2.0, Evaluate(8, 3, 0.000000000001));
+        }
+
+        [TestMethod]
+        public void Evaluation_PosNumberPosPowerWrongAccuracy_Zero()
+        {
+            Assert.AreEqual(0.0, Evaluate(2, 3, 0));
+        }
+
+        [TestMethod]
+        public void Evaluation_NegNumberEvenPowerWriteAccuracy_Result()
+        {
+            Assert.AreEqual(-2.0, Evaluate(-8, 3, 0.000000000001));
+        }
+
+        [TestMethod]
+        public void Evaluation_NegNumberOddPowerWriteAccuracy_Zero()
+        {
+            Assert.AreEqual(0.0, Evaluate(-8, 4, 0.000000000001));
+        }
+    }
+}
---
+++ Evaluation/Calculation.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evaluation
+{
+    public static class Calculation
+    {
+        public static double Evaluate(double number, int power, double accuracy)
+        {
+            if (IsPositive(power) && IsCorrectAccuracy(accuracy))
+            {
+                if(IsEven(power))
+                {
+                    if (number > 0) return MakeEvaluation(number, power, accuracy);
+                    else return 0.0;
+                }
+                else
+                {
+                    return MakeEvaluation(number, power, accuracy);
+                }
+            }
+            else return 0.0;
+        }
+
+        private static double MakeEvaluation(double number, double power, double accuracy)
+        {
+            double result = 1;
+
+            while (true)
+            {
+                double currentValue = 1 / power * ((power - 1) * result + number / (Math.Pow(result, power - 1)));
+                if (Math.Abs(result - currentValue) < accuracy) break;
+                result = currentValue;
+            }
+
+            return result;
+        }
+
+        private static bool IsCorrectAccuracy(double accuracy)
+        {
+            if (accuracy > 0 && accuracy < 1) return true;
+            else return false;
+        }
+
+        private static bool IsEven(int number)
+        {
+            if (number % 2 == 0) return true;
+            else return false;
+        }
+
+        private static bool IsPositive(int number)
+        {
+            if (number >= 0) return true;
+            else return false;
+        }
+    }
+}
